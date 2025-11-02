acff7790cd5ef779035a74b2e64e35a3624c2ea2
Ahmed Beltagy <kareembeltagy624@gmail.com>
2025-11-03 00:11:17 +0200

    Initial commit - project setup

---
+++ ContactsBusinessLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ContactsBusinessLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContactsBusinessLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3a7ab3d-3e8a-4001-b97d-69047fe9a115")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ contacts-ado-three-tier/Program.cs
@@ -0,0 +1,56 @@
+﻿using ContactsBusinessLayer;
+using System;
+using System.Data;
+using System.Diagnostics.Contracts;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void TestFindContact(int ID)
+        {
+
+            clsContact contact = clsContact.Find(ID);
+            if (contact != null)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine(contact.Email);
+                Console.WriteLine(contact.Phone);
+                Console.WriteLine(contact.Address);
+                Console.WriteLine(contact.DateOfBirth);
+                Console.WriteLine(contact.CountryID);
+                Console.WriteLine(contact.ImagePath);
+            }
+            else
+            { Console.WriteLine($"Contact {ID} not Found! "); }
+        }
+
+        static void TestAddNewContact()
+        {
+            clsContact Contact1=new clsContact();
+            Contact1.FirstName = "Fadi";
+            Contact1.LastName = "Maher";
+            Contact1.Email = "A@a.com";
+            Contact1.Phone = "010010";
+            Contact1.Address = "address1";
+            Contact1.DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0);
+            Contact1.CountryID = 1;
+            Contact1.ImagePath = "";
+
+            if (Contact1.Save())
+            {
+
+                Console.WriteLine("Contact Added Successfully with id=" + Contact1.ID);
+            }
+          
+
+        }
+        static void Main(string[] args)
+        {
+           TestFindContact(31);  //Not found!
+
+            TestAddNewContact();   
+
+        }
+    }
+}
